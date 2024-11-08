e5c33657986c691a33e599669301a57f06c93a07
shouzi.lzh <shouzi.lzh@digital-engine.com>
2024-11-08 20:57:49 +0800

    remove other page

--- src/router/MainRouter.tsx
+++ src/router/MainRouter.tsx
@@ -3,21 +3,11 @@ import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import React from 'react';
 
 const Home = lazy(() => import("../pages/home/home"));
-const Login = lazy(() => import("../pages/login/login"));
-const SignUp = lazy(() => import("../pages/login/sign-up/signUp"));
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <Home/>        // children:
-    },
-    {
-        path: '/login',
-        element: <Login/>
-    },
-    {
-        path: '/signUp',
-        element: <SignUp/>
     }
 ])
 
